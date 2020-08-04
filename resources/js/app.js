684f1eb2772c3a2fe5c70761ba08cc4f40295b16
Leyris Théolise <5374935-theoliseLey@users.noreply.gitlab.com>
2020-08-04 16:52:13 +0200

    Install bootstrap-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import BootstrapVue from "bootstrap-vue";
 import routes from './routes';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 let app = new Vue({
     el: '#app',
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
        <p>Home</p>
+       <b-button>Button</b-button>
     </div>
 </template>
 
