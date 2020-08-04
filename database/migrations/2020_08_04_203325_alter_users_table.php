a8e642d708ed570984c7f3cb9e0ec35692a8e17b
Leyris Théolise <5374935-theoliseLey@users.noreply.gitlab.com>
2020-08-04 22:42:45 +0200

    Create migration users table

---
+++ database/migrations/2020_08_04_203325_alter_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('email_verified_at');
+            $table->dropColumn('password');
+            $table->dropColumn('remember_token');
+            $table->dropColumn('name');
+            $table->string('username')->unique();
+            $table->integer('score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('score');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+        });
+    }
+}
